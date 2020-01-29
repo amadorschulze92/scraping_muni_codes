6da224771a051d74f146a53a6543d91f0f525f42
kjafshar <kjafshar@dons.usfca.edu>
2020-01-28 19:01:10 -0800

    1. converted mcode scraper to text extraciton vs download 2. configured mcode scraper to run one url at a time 3. configured kja_executor to run urls one at at time 4. built basic s3 writing functionality

---
+++ kja_scrape_executor.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+import scrape_codepub
+import scrape_qcode
+import muni_code_scraper
+from time import sleep
+import os
+import glob
+
+
+def rerun(my_funct, s3_bucket, s3_path, s3_table, base_loc, muni_tuple):
+
+    miss = my_funct(s3_bucket, s3_path, s3_table, base_loc, muni_tuple[1])
+    sleep(2)
+    if miss:
+        print(f'{muni_tuple[0]} has failed, rerunning')
+        final = my_funct(s3_bucket, s3_path, s3_table, base_loc, muni_tuple[1])
+        if len(final) > 0:
+            print(f'{muni_tuple[0]} has failed again')
+            return f'{muni_tuple[0]}: {muni_tuple[1]}'
+        else:
+            print('written successfully after second run')
+    else:
+        print(f"{muni_tuple[0]} completed")
+
+
+def s3_status_check(S3_bucket, S3_path):
+    return True
+
+
+def main():
+    og_df = pd.read_csv("my_links.csv", converters={'links': eval})
+    og_df = og_df.drop("Unnamed: 0", axis=1)
+
+    tuples_muni = muni_code_scraper.generate_municode_links()
+    df_codepub = og_df.loc[og_df["link_type"] == "codepub"]
+    df_qcode = og_df.loc[og_df["link_type"] == "qcode"]
+    df_amlegal = og_df.loc[og_df["link_type"] == "amlegal"]
+    df_other = og_df.loc[og_df["link_type"] == "other"]
+
+    codepub = zip(df_codepub["city"], df_codepub["links"])
+    qcode = zip(df_qcode["city"], df_qcode["links"])
+
+    base_loc = '/Users/kjafshar/Documents/test_folder/'
+
+    s3_bucket = 'mtc-redshift-upload'
+
+    s3_path = "test_kjafshar/"
+
+    s3_table = s3_status_check(s3_bucket, s3_path)
+
+    missed_muni = []
+
+    sleep(2)
+
+    for m in tuples_muni[:1]:
+        missed = rerun(muni_code_scraper.municode_scraper, s3_bucket, s3_path, s3_table, base_loc, m)
+        if missed:
+            missed_muni.append(missed)
+        else:
+            print("municode links successfully crawled")
+
+    # missed_code_p = rerun(scrape_codepub.code_pub_main, codepub, base_loc)
+
+    # missed_q_code = rerun(scrape_qcode.q_code_main, qcode, base_loc)
+
+    if len(missed_muni) > 0:
+        for item in missed_muni:
+            print(item)
+
+
+if __name__ == '__main__':
+    main()
--- muni_code_scraper.py
+++ muni_code_scraper.py
@@ -1,152 +1,29 @@
 from selenium import webdriver
 from time import sleep
 import os
-import shutil
-import glob
-import textract
+from datetime import datetime
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import sys
+import getpass
+
+user = getpass.getuser()
+sys.dont_write_bytecode = True
+
+# Select one of the following paths:
+
+# for DataViz team members
+sys.path.insert(0, "../Utility Code/")
+
+# # for all other teams with access to the Utility Code folder
+# sys.path.insert(0, '/Users/{}/Box/Utility Code'.format(user))
+
+from utils_io import *
 
 
-def doc_downloader(driver, output_dir, download_path, muni, update_date,  names, last_box):
-    """
-    This function takes adriver object and a final index, then navigates
-    to the download tab and downloads 1-last_box docs in groups of n to avoid download limits
-
-    :param driver: driver object created in municode_scraper
-    :param download_path: path to download folder
-    :param output_dir: path to output directory
-    :param muni: municipality
-    :param update_date: date the page was updated
-    :param names: list of document names
-    :param last_box: index of last unique doc for muni
-    :return: nothing
-    """
-
-    [button for button in driver.find_elements_by_tag_name("button") if button.text.lower() == "close"][0].click()
-    for i in range(last_box):
-
-        sleep(2)
-
-        # identify doc download button to open download tab
-
-        [link for link in driver.find_elements_by_tag_name("a") if "download" in link.text.lower()][0].click()
-        time = 0
-        x = [link for link in driver.find_elements_by_class_name("expToc-selector")][0:last_box]
-        success = False
-        while not success and time <= 20:
-            try:
-                x[0].click()
-                success = True
-            except:
-                sleep(1)
-                time += 1
-                x = [link for link in driver.find_elements_by_class_name("expToc-selector")][0:last_box]
-
-        x = x[i]
-        print(f'{muni} - {x.text}')
-        x.click()
-
-        sleep(2)
-
-        # Click download button for docs
-
-        b = [button for button in driver.find_elements_by_tag_name("button") if button.text.lower() == "download"]
-
-        sleep(2)
-
-        b[0].click()
-
-        # check to see if downloads complete
-
-        print('waiting for dl')
-
-        time = 0
-        while len(glob.glob(download_path + '/' + '*.doc')) == 0 and time < 360:
-            sleep(3)
-            time += 3
-        assert len(glob.glob(download_path + '/' + '*.doc')) == 1, "dl time out"
-        print('dl complete')
-        print(f'took {time} seconds')
-
-        # attempt to close download tab if is open
-
-        try:
-            [button for button in driver.find_elements_by_tag_name("button") if button.text.lower() == "close"][0].click()
-        except:
-            pass
-
-        # call file writer to convert word doc into named txt file
-
-        file_writer(download_path, output_dir, i, muni, update_date, names)
-
-
-def file_writer(download_path, output_dir, i, muni, update_date, names):
-    """
-    This function will combine downloaded docs into a single txt
-
-    :param download_path: path to download folder
-    :param output_dir: path to output directory
-    :param i: given index of doc
-    :param muni: municipality name
-    :param update_date: data municode updated
-    :param names: list of article names for given muni
-    :return: nothing
-    """
-
-    city_output = output_dir + muni + '/'
-    time_output = city_output + update_date + '/'
-    doc = glob.glob(download_path + '/' + '*.doc')[0]
-    # check to see if muni exists in results
-    # if not make a dir for the muni
-
-    if city_output not in glob.glob(output_dir + '*/'):
-        os.mkdir(city_output)
-
-    # write file inside muni/update_date
-
-    if time_output not in glob.glob(output_dir + '*/*/'):
-        os.mkdir(time_output)
-
-    filename = time_output + names[i] + '.txt'
-    try:
-        """
-        check redshift for most recent example of doc, extract text and compare
-        there may be issues in when update date changes
-        """
-
-
-        # text = str(textract.process(doc))
-        # if text diffs from previous text:
-        # with open(filename, 'w') as f:
-        #       f.write(text)
-
-        if filename not in glob.glob(time_output + '*.txt'):
-            text = str(textract.process(doc))
-
-            with open(filename, 'w') as f:
-                f.write(text)
-
-            # move downloads
-            shutil.move(doc, filename[:-4] + ".doc")
-
-            print('file write complete')
-            print('')
-        else:
-            print('doc already exists')
-            print('')
-    except:
-        print(f'file issue for {filename}')
-        print('')
-
-    # remove any docs left in download folder
-
-    dls = glob.glob(download_path + '/' + '*.doc')
-
-    for doc in dls:
-        os.remove(doc)
-
-
-def municode_scraper(base_loc, spec_ind=None):
-
+def generate_municode_links():
     city_county = 'Alameda County,Contra Costa County,Marin County,Napa County,City and County of San Francisco,San Mateo County,' \
                   'Santa Clara County,' \
                   'Solano County,Sonoma County,Alameda,Albany,Berkeley,Dublin,Emeryville,Fremont,Hayward,Livermore,Newark,Oakland,Piedmont,' \
@@ -163,142 +40,186 @@ def municode_scraper(base_loc, spec_ind=None):
                   'Suisun City,Vacaville,Vallejo,Cloverdale,Cotati,Healdsburg,Petaluma,Rohnert Park,Santa Rosa,Sebastopol,Sonoma,Windsor'
 
     city_county = city_county.split(",")
-    # home_folder = '/Users/kjafshar/Documents/'
-    download_path = base_loc + 'test_folder/'
-    output_dir = download_path + 'results/'
 
-    if download_path not in glob.glob(base_loc + '*/'):
-        os.mkdir(download_path)
-
-    if output_dir not in glob.glob(download_path + '/*/'):
-        os.mkdir(output_dir)
-
-    # enables setting of download folder
-
-    chrome_options = webdriver.ChromeOptions()
-    prefs = {'download.default_directory': download_path,
-             'profile.default_content_setting_values.automatic_downloads': 1,
-             'download.prompt_for_download': 'False'}
-
-    # configure multiple file download
-
-    chrome_options.add_experimental_option('prefs', prefs)
-    chrome_options.add_argument('--no-proxy-server')
-
-    driver = webdriver.Chrome('chromedriver', options=chrome_options)
+    driver = webdriver.Chrome('/Users/kjafshar/dev/MTC-Work/chromeDRIVER')
     driver.get('https://library.municode.com/ca')
 
-    sleep(5)
+    element = WebDriverWait(driver, 20) \
+        .until(EC.element_to_be_clickable((By.CSS_SELECTOR, "li[ng-repeat='client in letterGroup.clients']")))
 
-    ca_links_stale = driver.find_elements_by_tag_name("li")
-    muni_links_stale = [link for link in ca_links_stale if link.text in city_county]
-    muni_names = [link.text for link in muni_links_stale]
-    z = len(muni_names)
+    ca_links = driver.find_elements_by_tag_name("li")
 
-    if spec_ind:
-        x = spec_ind
-    else:
-        x = range(z)
+    # filter to desired municipalities
+    muni_links = [link for link in ca_links if link.text in city_county]
 
-    # create set for potential failed values
-
-    failed = set()
-
-    # attempt to crawl for given muni, if any failure occurs delete unconverted docs and open a new driver at the muni listing page
-
-    for i in x:
-        try:
-            div_page = False
-
-            # first gather fresh municipalities links
-            ca_links = driver.find_elements_by_tag_name("li")
-
-            # filter to desired municipalities
-            muni_links = [link for link in ca_links if link.text in city_county]
-
-            # enter municipality page
-            muni_links[i].click()
-            # allow page to load
-            sleep(3)
-
-            # check municipality name
-            muni = driver.current_url.split('/')[4]
-            print(i, muni)
-            print('')
-
-            # check for a division first before exposing docs
-
-            try:
-                update_date = driver.find_element_by_class_name("product-date").text  # update data only visible on actual code page, works as a check
-            except:
-                x = ([link for link in driver.find_elements_by_tag_name("li")
-                      if "municipal" in link.text.lower() or "ordinance" in link.text.lower()])[0]
-
-                x.find_elements_by_tag_name("a")[0].click()
-                div_page = True
-                sleep(1)
-                update_date = driver.find_element_by_class_name("product-date").text
-
-            # format update date
-
-            update_date = update_date.split(' ')[-3:]
-            update_date = datetime.strptime("-".join(update_date), '%B-%d-%Y').date()
-            update_date = update_date.strftime('%m-%d-%y')
-
-            # check for popup-window
-            sleep(1)
-            popup_button = [button for button in driver.find_elements_by_tag_name("button") if button.text.lower() == 'close']
-            if popup_button:
-                popup_button[0].click()
-
-            # need to click on a doc first to expose download button
-            base_doc = [link for link in driver.find_element_by_css_selector("section[id='toc']").find_elements_by_tag_name("li")]
-            base_doc[0].click()
-            sleep(2)
-            [link for link in driver.find_elements_by_tag_name("a") if "download" in link.text.lower()][0].click()
-            # wait for download tab to load
-            sleep(20)
-
-            # determine where list of unique docs ends
-
-            names = [link.text.lower() for link in driver.find_elements_by_class_name("expToc-selector")]
-            last_box = 0
-            for j, name in enumerate(names):
-                if name in names[:j]:
-                    last_box = j-1
-                    break
-
-            if last_box == 0:
-                last_box = len(names)
-
-            # download docs; last_box is end index for files, n is group size
-
-            doc_downloader(driver, output_dir, download_path, muni, update_date,  names, last_box)
-
-            # create named files
-
-            print(f'finished files for {muni}')
-
-            # if page had division early on it two backs
-
-            if div_page:
-                driver.back()
-            driver.back()
-            driver.back()
-        except:
-            print(f'{muni_names[i]} failed')
-            print('')
-
-            failed.add(i)
-            dls = glob.glob(download_path + '/' + '*.doc')
-
-            for doc in dls:
-                os.remove(doc)
-            driver.quit()
-            driver = webdriver.Chrome('chromedriver', options=chrome_options)
-            driver.get('https://library.municode.com/ca')
-
-            sleep(2)
+    muni_links = [(link.text, link.find_element_by_tag_name("a").get_attribute("href")) for link in muni_links]
 
     driver.quit()
-    return list(failed)
+
+    return muni_links
+
+
+
+def extract_text(driver):
+    filler_text = "\nSHARE LINK TO SECTION\nPRINT SECTION\nDOWNLOAD (DOCX) OF SECTIONS\nEMAIL SECTION"
+    element = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[ng-switch-when='CHUNKS']")))
+    doc = driver.find_element_by_css_selector('ul[class="chunks list-unstyled small-padding"]').text
+    doc = doc.replace(filler_text, '')
+    return doc
+
+
+def toc_crawler(driver):
+    smalltoc = driver.find_elements_by_css_selector("li[depth='-1']")
+    l_2_doc = []
+
+    for sub_chap in smalltoc:
+        sub_chap.click()
+        sleep(1)
+        if driver.find_elements_by_css_selector("div[ng-switch-when='CHUNKS']"):
+            l_2_doc.append(extract_text(driver))
+        elif driver.find_elements_by_css_selector("div[ng-switch-when='TOC']"):
+            level_3_doc = []
+            tinytoc = sub_chap.find_elements_by_css_selector("li[depth='-1']")
+            for subsub_chap in tinytoc:
+                sleep(1)
+                subsub_chap.click()
+                level_3_doc.append(extract_text(driver))
+            l_2_doc.append("\n".join(level_3_doc))
+
+    return "\n".join(l_2_doc)
+
+
+def page_crawler(driver, s3_bucket, s3_path, s3_table, base_loc, muni, update_date):
+    element = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "section[id='toc']")))
+    toc = [link for link in driver.find_element_by_css_selector("section[id='toc']").find_elements_by_tag_name("li")]
+    for chap in toc[:5]:
+        title = chap.text
+        print(title)
+        chap.click()
+
+        sleep(2)
+
+        if driver.find_elements_by_css_selector("div[ng-switch-when='CHUNKS']"):
+
+            s3_file_writer(s3_bucket, s3_path, s3_table, base_loc, muni, update_date, title, extract_text(driver))
+
+        elif driver.find_elements_by_css_selector("div[ng-switch-when='TOC']"):
+
+            s3_file_writer(s3_bucket, s3_path, s3_table, base_loc, muni, update_date, title, toc_crawler(driver))
+
+        else:
+
+            print(f'{muni}-{title} failed')
+
+        close_button = driver.find_elements_by_css_selector('i[class="fa-fw fa fa-chevron-down"]')
+        if close_button:
+            close_button[0].click()
+
+
+
+
+def check_for_s3_delta(muni, title, text, s3_table):
+    return True
+
+
+def s3_file_writer(s3_bucket, s3_path, s3_table, base_loc, muni, update_date, title, text):
+    """
+    This function will combine downloaded docs into a single txt
+
+    :param base_loc: path to download folder
+    :param output_dir: path to output directory
+    :param i: given index of doc
+    :param muni: municipality name
+    :param update_date: data municode updated
+    :param names: list of article names for given muni
+    :return: nothing
+    """
+
+    s3_key = (s3_path +
+              muni + "/" +
+              update_date + '/' +
+              title + ".txt")
+
+    filename = base_loc + title + '.txt'
+
+    if check_for_s3_delta(muni, title, text, s3_table):
+        with open(filename, 'w') as f:
+            f.write(text)
+
+        copy_file_to_s3(filename, s3_bucket, s3_key)
+        # write to s3
+        #except:
+        #print(f'file issue for {filename}')
+        #print('')
+        # os.remove(filename)
+        print('file write complete')
+        print('')
+    else:
+        print("doc hasn't changed")
+        print('')
+
+
+    # remove any docs left in download folder
+
+
+def municode_scraper(s3_bucket, s3_path, s3_table, base_loc, url):
+
+    driver = webdriver.Chrome('/Users/kjafshar/dev/MTC-Work/chromeDRIVER')
+    driver.get(url)
+
+    sleep(1)
+
+    # check municipality name
+    muni = driver.current_url.split('/')[4]
+    print(muni)
+    print('')
+    sleep(3)
+    # check for a division first before exposing docs
+
+    try:
+        update_date = driver.find_element_by_class_name("product-date").text  # update data only visible on actual code page, works as a check
+    except:
+        x = ([link for link in driver.find_elements_by_tag_name("li")
+              if "municipal" in link.text.lower() or "ordinance" in link.text.lower()])[0]
+
+        x.find_elements_by_tag_name("a")[0].click()
+        div_page = True
+        sleep(1)
+        update_date = driver.find_element_by_class_name("product-date").text
+
+    # format update date
+
+    update_date = update_date.split(' ')[-3:]
+
+    update_date = datetime.strptime("-".join(update_date), '%B-%d,-%Y').date()
+
+    update_date = update_date.strftime('%m-%d-%y')
+
+    # check for popup-window
+
+    try:
+        popup_button = WebDriverWait(driver, 5) \
+    .until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[class='hopscotch-bubble-close hopscotch-close']")))
+
+        popup_button.click()
+    except:
+        pass
+
+    try:
+        page_crawler(driver, s3_bucket, s3_path, s3_table, base_loc, muni, update_date)
+
+        # create named files
+
+        print(f'finished crawling for {muni}')
+
+        # if page had division early on it two backs
+
+        driver.quit()
+
+    except:
+
+        driver.quit()
+
+        return "failed"
+
+    driver.quit()
