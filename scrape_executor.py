cf0a532ef75970284590c190b477585333ddffd1
Michael Schulze <mschulze@Michaels-MacBook-Pro.local>
2020-02-03 08:53:35 -0800

    switching filenames

--- scrape_executor.py
+++ scrape_executor.py
@@ -1,43 +1,90 @@
 import pandas as pd
+import sys
 import numpy as np
-import scrape_codepub
-import scrape_qcode
+import codepub_scraper
+import qcode_scraper
 import muni_code_scraper
+from time import sleep
+from time import time
+import os
+import glob
 
 
-def rerun(my_funct, links, base_loc):
-    missed = my_funct(base_loc, links)
-    if len(missed) == 0:
-        return "All cities completed"
-    print("\nRe-run missing cities\n")
-    missed_again = my_funct(base_loc, missed)
-    return missed_again
+def rerun(my_funct, s3_bucket, s3_path, s3_table, base_loc, muni_tuple):
+
+    start = time()
+    miss = my_funct(s3_bucket, s3_path, s3_table, base_loc, muni_tuple)
+    sleep(2)
+
+    if miss:
+        print(f'{muni_tuple[0]} has failed, rerunning')
+        final = my_funct(s3_bucket, s3_path, s3_table, base_loc, muni_tuple)
+        if final:
+            print(f'{muni_tuple[0]} has failed again')
+            return f'{muni_tuple[0]}: {muni_tuple[1]}'
+        else:
+            print('written successfully after second run')
+    else:
+
+        print(f"{muni_tuple[0]} completed in {time() - start} seconds")
+
+
+def s3_status_check(S3_bucket, S3_path):
+    return True
 
 
 def main():
+
+    cwd = os.getcwd()
+
+    sys.path.insert(0,cwd + "/" + "chromedriver")
+
     og_df = pd.read_csv("my_links.csv", converters={'links': eval})
     og_df = og_df.drop("Unnamed: 0", axis=1)
 
-    df_muni = og_df.loc[og_df["link_type"] == "municode"]
+    tuples_muni = muni_code_scraper.generate_municode_links()
     df_codepub = og_df.loc[og_df["link_type"] == "codepub"]
     df_qcode = og_df.loc[og_df["link_type"] == "qcode"]
     df_amlegal = og_df.loc[og_df["link_type"] == "amlegal"]
     df_other = og_df.loc[og_df["link_type"] == "other"]
 
-    codepub = zip(df_codepub["city"], df_codepub["links"])
-    qcode = zip(df_qcode["city"], df_qcode["links"])
+    base_loc = '/Users/kjafshar/Documents/test_folder/'
 
-    base_loc = '/Users/mschulze/Documents/test_selenium/'
+    s3_bucket = 'mtc-redshift-upload'
 
-    missed_muni = rerun(muni_code_scraper.municode_scraper, [], base_loc)
-    print(missed_muni)
+    s3_path = "test_kjafshar/"
 
-    missed_code_p = rerun(scrape_codepub.code_pub_main, codepub, base_loc)
-    print(missed_code_p)
+    s3_table = s3_status_check(s3_bucket, s3_path)
 
-    missed_q_code = rerun(scrape_qcode.q_code_main, qcode, base_loc)
-    print(missed_q_code)
+    missed_municipal = []
+
+    sleep(2)
+
+    for m in tuples_muni[:1]:
+        missed_municode = rerun(muni_code_scraper.municode_scraper, s3_bucket, s3_path, s3_table, base_loc, m)
+        if missed_municode:
+            missed_municipal.append(missed_municode)
+        else:
+            print("municode links successfully crawled")
+
+    for city, link in zip(df_codepub["city"], df_codepub["links"]):
+        missed_codepub = rerun(codepub_scraper.code_pub_main, s3_bucket, s3_path, s3_table, base_loc, [city, link])
+        if missed_codepub:
+            missed_municipal.append(missed_codepub)
+        else:
+            print("code publishing links successfully crawled")
+
+    for city, link in zip(df_qcode["city"], df_qcode["links"]):
+        missed_qcode = rerun(qcode_scraper.q_code_main, s3_bucket, s3_path, s3_table, base_loc, [city, link])
+        if missed_qcode:
+            missed_municipal.append(missed_qcode)
+        else:
+            print("q code links successfully crawled")
+
+    if len(missed_municipal) > 0:
+        for item in missed_municipal:
+            print(item)
 
 
 if __name__ == '__main__':
-  main()
+    main()
